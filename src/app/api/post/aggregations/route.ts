9ba97e62c1630d6f9e135035e30f13ff4ebf744a
prasanv <34255378+prasanvb@users.noreply.github.com>
2024-10-13 17:45:38 +0000

    feat: post likes aggregations

---
+++ src/app/api/post/aggregations/route.ts
@@ -0,0 +1,25 @@
+import prisma from '@/lib/prisma';
+
+// route: api/post/aggregations
+
+export async function GET(req: Request) {
+  const aggregations = await prisma.post.aggregate({
+    _sum: {
+      likeNum: true,
+    },
+    _avg: {
+      likeNum: true,
+    },
+    _count: {
+      id: true,
+    },
+    _max: {
+      likeNum: true,
+    },
+    _min: {
+      likeNum: true,
+    },
+  });
+
+  return new Response(JSON.stringify(aggregations), { status: 200 });
+}
